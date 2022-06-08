1de6f074ec24d21fc3a5b658fd1ff005516bf4c0
F.Kanayama <fkanayam@redhat.com>
2022-06-09 01:00:28 +0900

    Skeleton

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,25 +1,5 @@
-const divisibleByN = (i, n) => i % n == 0;
-const divisibleBy3 = (i) => divisibleByN(i, 3);
-const divisibleBy5 = (i) => divisibleByN(i, 5);
 
-/**
- * returns Fizz, Buzz, Fizz Buzz, or number based on number passed.
- * @param {number} number 
- * @returns {string}
- */
-function fizzbuzz(number) {
-  if (!Number.isInteger(number)) throw `${number} is not an integer.`;
-  if (number <= 0) throw `${number} is not a positive integer`;
-
-  if (divisibleBy3(number) && divisibleBy5(number)) {
-    return 'Fizz Buzz';
-  } else if (divisibleBy3(number)) {
-    return 'Fizz';
-  } else if (divisibleBy5(number)) {
-    return 'Buzz';
-  } else {
-    return String(number);
-  }
+function fizzbuzz() {
 }
 
 export {fizzbuzz};
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -1,45 +1,11 @@
 import {fizzbuzz} from './FizzBuzz'
 
+function myfunc() {
+  // todo
+}
 
-/**
- * sum up numbers.
- * @type{function(...number): number}
- */
-const sum = (...nums) => nums.reduce((prev, current) => prev + current, 0);
-describe('sum', () => {
-  it('should sum up all numbers', () => {
-    expect(sum(1, 2, 3, 4)).toBe(10);
-    expect(sum()).toBe(0);
+describe('myfunc', () => {
+  it('should multiple 2 arguments.', () => {
+    expect(myfunc(/*something*/)).toBe(/*something*/);
   });
 })
-
-describe('fizzbuzz', () => {
-  it('should return "1" on 1', () => {
-    expect(fizzbuzz(1)).toBe('1');
-    expect(fizzbuzz(2)).toBe('2');
-    expect(fizzbuzz(4)).toBe('4');
-  });
-  it('should return Fizz on number divisible by 3', () => {
-    expect(fizzbuzz(3)).toBe('Fizz');
-    expect(fizzbuzz(3*2)).toBe('Fizz');
-    expect(fizzbuzz(3*3)).toBe('Fizz');
-  });
-  it('should return Buzz on number divisible by 5', () => {
-    expect(fizzbuzz(5)).toBe('Buzz');
-    expect(fizzbuzz(5*2)).toBe('Buzz');
-    expect(fizzbuzz(5*4)).toBe('Buzz');
-  });
-  it('should return Fizz Buzz on number divisible by 3 and 5', () => {
-    expect(fizzbuzz(5*3)).toBe('Fizz Buzz');
-    expect(fizzbuzz(5*3*2)).toBe('Fizz Buzz');
-    expect(fizzbuzz(5*3*3)).toBe('Fizz Buzz');
-  });
-  it('should throw an error on passing non-integer value', () => {
-    expect(() => fizzbuzz('1')).toThrow();
-    expect(() => fizzbuzz(0.1)).toThrow();
-  });
-  it('should throw an error on passing less than or equal to zero', () => {
-    expect(() => fizzbuzz(0)).toThrow();
-    expect(() => fizzbuzz(-1)).toThrow();
-  });
-});
