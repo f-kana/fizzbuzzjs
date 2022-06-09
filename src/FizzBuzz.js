b4f38047c13a637e8c33a5263d5eb878231bc240
F.Kanayama <fkanayam@redhat.com>
2022-06-09 13:12:22 +0900

    Workshop was completed

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,53 @@
 
-function fizzbuzz() {
+// A. Intの引数を１つもち、戻り値String型の関数を作る
+// B-1. 引数チェックで1~100の場合は"Error"ではない値を返却する
+// B-2. 引数チェックで1~100以外の場合は"Error"という文字列を返す
+// C. 3の倍数かつ5の倍数の場合、FizzBuzzを返却する。
+// D. 3の倍数であり5の倍数でない場合、Fizzを返却する。
+// E. 5の倍数であり3の倍数でない場合、Buzzを返却する。
+
+// F. 3の倍数でも5の倍数でも無い場合、引数をString型に変換して返却する。
+
+function divisibleByN(num, n) {
+  return num % n === 0;
+}
+
+function divisibleBy3(num) {
+  // return num % 3 === 0;
+  return divisibleByN(num, 3);
+}
+
+const divisibleBy5 = (num) => num % 5 === 0;
+
+function fizzbuzz(num) {
+  const ERROR_MESSAGE = "Error";
+  const FIZZ = "Fizz";
+  const BUZZ = "Buzz";
+  const MIN_NUM = 1;
+  const MAX_NUM = 100;
+
+  if (arguments.length !== 1) {
+    return ERROR_MESSAGE;
+  }
+
+  if (!Number.isInteger(num)) {
+    return ERROR_MESSAGE;
+  }
+
+  if (num < MIN_NUM || num > MAX_NUM) {
+    return ERROR_MESSAGE;
+  }
+
+  if (divisibleBy3(num) && divisibleBy5(num)) {
+    return FIZZ + BUZZ;
+  } else if (divisibleBy3(num)) {
+    return FIZZ;
+  } else if (divisibleBy5(num)) {
+    return BUZZ;
+  }
+
+  return String(num);
 }
 
 export {fizzbuzz};
+
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -1,11 +1,53 @@
 import {fizzbuzz} from './FizzBuzz'
 
-function myfunc() {
-  // todo
+// todo: ２つの引数の積を返す
+
+function multiplier2numbers(num_1, num_2) {
+  return num_1 * num_2;
 }
 
-describe('myfunc', () => {
+describe('my test', () => {
   it('should multiple 2 arguments.', () => {
-    expect(myfunc(/*something*/)).toBe(/*something*/);
+    expect(multiplier2numbers(1, 2)).toBe(2);
   });
+})
+
+
+
+describe('fizzbuzz', () => {
+  it('Intの引数を１つもち、戻り値String型の関数を作る', () => {
+    expect(fizzbuzz(11)).toBe("11");
+  });
+  it('引数チェックで1~100の場合は"Error"ではない値を返却する', () => {
+    expect(fizzbuzz(1)).not.toBe("Error");
+    expect(fizzbuzz(100)).not.toBe("Error");
+  });
+  it('引数チェックで1~100以外の場合は"Error"という文字列を返す', () => {
+    expect(fizzbuzz(101)).toBe("Error");
+    expect(fizzbuzz(0)).toBe("Error");
+    expect(fizzbuzz()).toBe("Error");
+    expect(fizzbuzz(1.5)).toBe("Error");
+    expect(fizzbuzz(1,2,3,4,5,6)).toBe("Error");
+    expect(fizzbuzz("aaa")).toBe("Error");
+  });
+
+  it('3の倍数かつ5の倍数の場合、FizzBuzzを返却する。', () => {
+    expect(fizzbuzz(15)).toBe("FizzBuzz");
+  });
+
+  it('3の倍数であり5の倍数でない場合、Fizzを返却する。', () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
+    expect(fizzbuzz(6)).toBe("Fizz");
+  });
+
+  it('5の倍数であり3の倍数でない場合、Buzzを返却する。', () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
+    expect(fizzbuzz(10)).toBe("Buzz");
+  });
+
+  it('3の倍数でも5の倍数でも無い場合、引数をString型に変換して返却する。', () => {
+    expect(fizzbuzz(2)).toBe("2");
+    expect(fizzbuzz(7)).toBe("7");
+  });
+  
 })
